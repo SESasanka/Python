1ab12cf4cd143a30b4cbccaeb391b8c101c055bb
SESasanka <157799189+SESasanka@users.noreply.github.com>
2024-05-14 22:19:40 +0530

    main

---
+++ ictfr.py
@@ -0,0 +1,10 @@
+ictfr = 1012 + 0.5 / True - False + True
+omabc = 1010 + 0.5 / True - False + True
+print(ictfr+omabc)
+
+# rc = True - False + True
+# print(rc)
+
+# ictfr = 1012 + 0.5 
+# omabc = 1010 + 0.5
+# print(ictfr+omabc)
