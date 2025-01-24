1d04c048bcbc464e9b830f730a0444977d867969
SESasanka <sasankaakash22@gmail.com>
2025-01-24 22:14:29 +0530

    main

--- text.py
+++ text.py
@@ -1,31 +1,31 @@
-# x = [i for i in range(10) if i % 2 == 0]
+x = [i for i in range(10) if i % 2 == 0]
 
-# print(x)
-
-
-# def complicated_fun(*args, **kwargs):
-#     print(args,kwargs)
-#     pass
-
-# complicated_fun(x=1,s = "hello",b=True)
-# def masking(number,mask):
-
-#     str = ''
-#     n = mask
-
-#     for x in number[:-n]:
-#         str +='*'
-#     str += number[-n:]
-#     return str
-# print(masking("123456789",4))
+print(x)
 
 
-# str = "123456789"
+def complicated_fun(*args, **kwargs):
+    print(args,kwargs)
+    pass
 
-# str_len = len(str)
-# mask = str_len - 4
-# showerstr = str[mask:]
+complicated_fun(x=1,s = "hello",b=True)
+def masking(number,mask):
 
-# print("*" * mask + showerstr)
+    str = ''
+    n = mask
+
+    for x in number[:-n]:
+        str +='*'
+    str += number[-n:]
+    return str
+print(masking("123456789",4))
+
+
+str = "123456789"
+
+str_len = len(str)
+mask = str_len - 4
+showerstr = str[mask:]
+
+print("*" * mask + showerstr)
 
 
