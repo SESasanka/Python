86ea32ac0b1994878d4d69e8a89ffc37236e6b04
SESasanka <sasankaakash22@gmail.com>
2025-01-24 22:38:32 +0530

    main

--- pattern.py
+++ pattern.py
@@ -1,9 +1,30 @@
-def myPattern(n):
+# def myPattern(n):
       
-    for i in range (0,n):
-        for j in range (0, i +1):
-            print("*", end="")
-        print("\r")
+#     for i in range (0,n):
+#         for j in range (0, i +1):
+#             print("*", end="")
+#         print("\r")
 
-n = 5
-myPattern(n)
+# n = 5
+# myPattern(n)
+
+
+def hide_card_num(number,mask):
+    str = ''
+    n = mask
+
+    for i in number[:-n]:
+        str +='*'
+    str += number[-n:]
+    return str
+
+print(hide_card_num("12365745367353", 4))
+
+
+# str = "123456789"
+
+# str_len = len(str)
+# mask =str_len - 4
+# showerstr = str[mask:]
+
+# print("*" * mask +  showerstr)
