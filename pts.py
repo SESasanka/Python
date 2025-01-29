032464b9bc827bce9b14df7732df97e16bf3af9d
SESasanka <sasankaakash22@gmail.com>
2025-01-29 12:45:22 +0530

    main

---
+++ pts.py
@@ -0,0 +1,19 @@
+n = 5
+for i in range(n - 1):
+    for j in range(i,n):
+        print("   ", end="")
+    for j in range(i):
+        print(" * ",end="")
+    for j in range(i+1):
+        print(" * ",end="")
+    print()
+    
+for i in range(n):
+    for j in range(i + 1):
+        print("   ", end="")
+    for j in range(i,n-1):
+        print(" * ",end="")
+    for j in range(i,n):
+        print(" * ",end="")
+    print()
+    
