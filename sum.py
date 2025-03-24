12c967db25541b49ce0b78d53dfc9376471364c4
SESasanka <sasankaakash22@gmail.com>
2025-03-24 20:04:15 +0530

    sum

--- sum.py
+++ sum.py
@@ -1,37 +1,37 @@
-# numbers = [float(input(f"Enter number {i+1}: ")) for i in range(10)]
-# print("The sum of the ten numbers is:", sum(numbers))
+numbers = [float(input(f"Enter number {i+1}: ")) for i in range(10)]
+print("The sum of the ten numbers is:", sum(numbers))
 
-# numbers = [float(input(f"Enter number {i+1}: ")) for i in range(10)]
-# print("The maximum number is:", max(numbers))
+numbers = [float(input(f"Enter number {i+1}: ")) for i in range(10)]
+print("The maximum number is:", max(numbers))
 
-# def factorial(n):
-#     fact = 1
-#     for i in range(1, n + 1):
-#         fact *= i
-#     return fact
+def factorial(n):
+    fact = 1
+    for i in range(1, n + 1):
+        fact *= i
+    return fact
 
-# for i in range(5):
-#     num = int(input(f"Enter number {i+1}: "))
-#     if num < 0:
-#         print("Factorial is not defined for negative numbers.")
-#     else:
-#         print(f"Factorial of {num} is {factorial(num)}")
+for i in range(5):
+    num = int(input(f"Enter number {i+1}: "))
+    if num < 0:
+        print("Factorial is not defined for negative numbers.")
+    else:
+        print(f"Factorial of {num} is {factorial(num)}")
 
-# Function to check if a number is prime
-# def is_prime(n):
-#     if n < 2:
-#         return False
-#     for i in range(2, n):  # Check divisibility from 2 to n-1
-#         if n % i == 0:
-#             return False
-#     return True
 
-# # User input
-# num = int(input("Enter a number: "))
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, n):  # Check divisibility from 2 to n-1
+        if n % i == 0:
+            return False
+    return True
 
-# # Check if prime
-# if is_prime(num):
-#     print(f"{num} is a Prime number.")
-# else:
-#     print(f"{num} is NOT a Prime number.")
+# User input
+num = int(input("Enter a number: "))
+
+# Check if prime
+if is_prime(num):
+    print(f"{num} is a Prime number.")
+else:
+    print(f"{num} is NOT a Prime number.")
 
