ae8214f1f8834927b2a85027bdd95f696878de27
SESasanka <sasankaakash22@gmail.com>
2025-05-07 21:37:03 +0530

    decus of IT clz

---
+++ decus.py
@@ -0,0 +1,21 @@
+# count = 0
+# while count < 10:
+#     print("Welcome to Python")
+#     count +=1
+
+# ------------------------------------
+
+# x = 10
+
+# x = x+4
+# x = x+4-x
+# x = x-(x+4)
+# print(x)
+
+# ------------------------------------
+
+balance = 10
+while True:
+    if balance < 9: pass
+    balance=balance-9
+    print(balance)
