d7fb0475781ee4b4c85a1914e5da7a785a9e61b6
SESasanka <sasankaakash22@gmail.com>
2025-06-11 20:18:37 +0530

    pritice

---
+++ prictice.py
@@ -0,0 +1,93 @@
+# n = 5
+
+def tri_recursion(k):
+  if(k > 0):
+    result = k + tri_recursion(k - 1)
+    print(result)
+  else:
+    result = 0
+  return result
+
+print("Recursion Example Results:")
+tri_recursion(6)
+
+
+
+
+
+# for i in range(n-1):
+#     for j in range(i,n):
+#          print("   ", end="")
+#     for j in range(i):
+#          print(" * ", end="")
+#     for j in range(i+1):
+#          print(" * ", end="")
+#     print()
+
+# for i in range(n):
+#     for j in range(i+1):
+#          print("   ", end="")
+#     for j in range(i,n-1):
+#          print(" * ", end="")
+#     for j in range(i,n):
+#          print(" * ", end="")
+#     print()
+
+
+# for i in range(n-1):
+#     for j in range(i,n):
+#         print("  ",end='')
+#     for j in range(i):
+#         print(" *",end='')
+#     for j in range(i+1):
+#         print(" *",end='')
+#     print()
+# for i in range(n):
+#     for j in range(i+1):
+#         print("  ",end='')
+#     for j in range(i,n-1):
+#         print(" *",end='')
+#     for j in range(i,n):
+#         print(" *",end='')
+#     print()
+
+
+# for i in range(n):
+#     for j in range(i+1):
+#         print(" *", end='')
+#     for j in range(i, n):
+#         print("  ", end='')
+#     for j in range(i, n-1):
+#         print("  ", end='')
+#     for j in range(i+1):
+#         print(" *", end='')
+#     print()
+# for i in range(n):
+#     for j in range(i,n):
+#          print(" *", end="")
+#     for j in range(i+1):
+#          print("  ", end="")
+#     for j in range(i):
+#          print("  ", end="")
+#     for j in range(i,n):
+#          print(" *", end="")
+#     print()
+
+
+# for i in range(n-1):
+#     for j in range(i,n):
+#         print("  ",end='')
+#     for j in range(i):
+#         print(" *",end='')
+#     for j in range(i+1):
+#         print(" *",end='')
+#     print()
+
+# for i in range(n):
+#     for j in range(i+1):
+#         print("  ",end='')
+#     for j in range(i,n-1):
+#         print(" *",end='')
+#     for j in range(i,n):
+#         print(" *",end='')
+#     print()
