2269505ba5f745f512193cab404664557f55c32e
SESasanka <sasankaakash22@gmail.com>
2025-07-26 00:47:34 +0530

    Comment out unused function and variable in test.py for clarity

--- test.py
+++ test.py
@@ -12,13 +12,24 @@
 # print(newlist)
 
 
-list_a =[5,15,20,16,8]
+# list_a =[5,15,20,16,8]
 
-def f(x):
-    total = 0
-    for i in x:
-        total = total+i
-        if total >= 50:
-            break
-    return (total)
-print(f(list_a))
+# def f(x):
+#     total = 0
+#     for i in x:
+#         total = total+i
+#         if total >= 50:
+#             break
+#     return (total)
+# print(f(list_a))
+
+
+mylist = [64, 34, 25, 12, 22, 11, 90, 5]
+
+n = len(mylist)
+for i in range(n-1):
+  for j in range(n-i-1):
+    if mylist[j] > mylist[j+1]:
+      mylist[j], mylist[j+1] = mylist[j+1], mylist[j]
+
+print(mylist)
