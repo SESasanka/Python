cf39f5199d38aae252eee16745bd674f53b462da
SESasanka <sasankaakash22@gmail.com>
2025-07-28 00:13:56 +0530

    Comment out unused code in test.py for clarity and organization

--- test.py
+++ test.py
@@ -24,12 +24,106 @@
 # print(f(list_a))
 
 
-mylist = [64, 34, 25, 12, 22, 11, 90, 5]
+# mylist = [64, 34, 25, 12, 22, 11, 90, 5]
 
-n = len(mylist)
-for i in range(n-1):
-  for j in range(n-i-1):
-    if mylist[j] > mylist[j+1]:
-      mylist[j], mylist[j+1] = mylist[j+1], mylist[j]
+# n = len(mylist)
+# for i in range(n-1):
+#   for j in range(n-i-1):
+#     if mylist[j] > mylist[j+1]:
+#       mylist[j], mylist[j+1] = mylist[j+1], mylist[j]
 
-print(mylist)
+# print(mylist)
+
+
+# even = 0
+# count = 0
+# x = 2
+
+# while x < 10:
+#     even += 2
+#     # print(even)
+#     count = count+even
+#     x+=2
+# print(f"count {count}")
+
+# count = 0
+# i =1
+
+# for i in range(i,10):
+#     inputNum = int(input(f"Enter Number {i} : "))
+
+#     count += inputNum
+# print(f"Ten numbers count is {count}")
+
+
+# fact = 0
+# num = 0
+
+# num = int(input("Enter Number : "))
+
+# fact = num
+
+# while num > 1:
+#     num -= 1
+#     fact *=num
+# print(fact)
+
+
+# num = int(input("Enter Number: "))
+
+# if num <= 1:
+#     print("This number is not Prime")
+# else:
+#     i = 2
+#     is_prime = True
+
+#     while i < num:
+#         if num % i == 0:
+#             is_prime = False
+#             break
+#         i += 1
+
+#     if is_prime:
+#         print("This number is Prime")
+#     else:
+#         print("This number is not Prime")
+
+
+# def factorial(n):
+#     fact = 1
+#     for i in range(1, n+1):
+#         fact *= i
+#     return fact
+
+
+# num = int(input("Enter number : "))
+# if num < 0:
+#     print("Factorial is not defiend")
+# else:
+#     print(f"Factorial of {num} is {factorial(num)}")
+
+
+# rs = 0
+# ar = []
+# for i in range(1,5):
+#     rs = int(input(f"Enter Result {i} : "))
+#     ar.append(rs)
+# print(ar)
+# print(len(ar))
+
+# tot = 0
+
+# for i in range(1,11):
+#     num = int(input(f"Enter Number {i} : "))
+#     tot+=num
+# print(f"Total of ten number is {tot}")
+
+
+l = [2,5,4,3,1]
+
+for i in range(len(l)):
+    for j in range(len(l)-1):
+      if  l[j] < l[j+1]:
+         l[j],l[j+1]=l[j+1],l[j]
+
+print(l)
