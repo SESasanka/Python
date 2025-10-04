f5e8775af2a84108aa87dcb7f998dff8188f69b1
SESasanka <sasankaakash22@gmail.com>
2025-10-04 13:10:41 +0530

    Add examples of for loops, range usage, and bubble sort implementation

--- Gpython/text.py
+++ Gpython/text.py
@@ -283,7 +283,70 @@
 #   print(i)   
 
 
-# x = 1234567890
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#   print(x)
 
-# y = str(x)
-# print(y[2])
+# for x in "banana":
+#   print(x)
+
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#   print(x)                    ---> apple , banana
+#   if x == "banana":
+#     break
+
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#   if x == "banana":
+#     break
+#   print(x) <-----apple
+
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#   if x == "banana":
+#     continue
+#   print(x)   ------>apple,cherry
+
+# for x in range(1,6):
+#   print(x)     1,2,3,4,5
+
+# for x in range(2, 6):
+#   print(x)    2,3,4,5
+
+# for x in range(2, 30, 3):
+#   print(x)        2,5,8,11,14,17,20,23,26,29
+
+# for x in range(6):
+#   if x == 3: 
+#     break
+#   print(x)
+# else:
+#   print("Finally finished!")  012
+
+
+# n = 5
+
+# for i in range(n):  #column
+#     for j in range(i+1) :  #row
+#         print(' *',end='')
+#     print()  
+
+# *
+# * *
+# * * *
+# * * * *
+# * * * * *
+
+# for x in [0, 1, 2]:
+#   pass
+
+
+x = [7, 9, 12, 11, 3]
+
+for i in range (0,len(x)):
+    for j in range(0,len(x)-1):
+        if x[j] > x[j+1]:
+            x[j],x[j+1] = x[j+1],x[j]
+
+print(x)
